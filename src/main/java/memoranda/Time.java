526ce50b26b49e4b0b7dcc477c83c749e4deb04e
JoshLSloan <jsloan13@asu.edu>
2018-11-27 23:23:07 -0700

    Initial Commit

---
+++ src/main/java/memoranda/Time.java
@@ -0,0 +1,22 @@
+package main.java.memoranda;
+
+public class Time {
+
+    private int hour;
+    private int minute;
+    
+    public Time(int parmHour, int parmMinute) {
+        this.hour = parmHour;
+        this.minute = parmMinute;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+    
+    
+}
